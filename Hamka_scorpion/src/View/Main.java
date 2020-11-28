d3afb61d58355f9a1cc925bede6afc497dbe9e44
Mutlaq Hijazi <53138763+Mutlaq0@users.noreply.github.com>
2020-11-29 00:24:40 +0200

    Update Main.java

--- Hamka_scorpion/src/View/Main.java
+++ Hamka_scorpion/src/View/Main.java
@@ -1,26 +1,27 @@
 package View;
 
+import Model.Game;
+
 public class Main  {
 
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 //		Winner w1 = new Winner("Mohamed",720,"20/07/2019");
 //		Winner w2 = new Winner("Lool",400,"16/11/2020");
 //		SysData.getInstance().addWinnerToLeaderboard(w1);
+//		Winner w2 = new Winner("Lool",400,"16/11/2020");
 //		SysData.getInstance().addWinnerToLeaderboard(w2);
-//		SysData.getInstance().writeWinnersIntoFile();
-		System.out.println("SDSD");
+//		SysData.getInstance().addWinnerToLeaderboard(w1);
 //		Game game = new Game("PL","SS");
+		System.out.println("SDSD");
 //		game.getPossibleMovesForWhiteSoldier(1, game.getPair(2, 1));
 		//game.moveBlackSoldier(game.getPair(4, 3), game.getPair(6, 1), game.getPossibleMovesForBlackSoldier(game.getContentWithXandY(4,3), game.getPair(4,3)));
 //		System.out.println(game.getblackPlayerSoldiers());
-
 //		System.out.println(game.generateYellowTiles());
 //		game.getQueenBiasMoves(game.getContentWithXandY(4, 3), game.getPair(4, 3));
 //		boolean leg = game.checkIfLegalPosition(1, game.getPair(4, 3));
 //		System.out.println(leg);
 //		game.getBiasMovesQueen(game.getContentWithXandY(2, 5), game.getPair(2, 5));
+		game.getBiasMovesQueen(game.getContentWithXandY(2, 5), game.getPair(2, 5));
 		
 	}
 }
